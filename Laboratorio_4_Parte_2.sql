8d73a65023592ba6b36b5ff898f61003d0ca571b
luis-jaramillo-talentotech <luis.jaramillo.ttv.356@unilibre.edu.co>
2024-11-06 20:41:52 -0500

    Create table 'autores'

--- Laboratorio_4_Parte_2.sql
+++ Laboratorio_4_Parte_2.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE biblioteca;
+
+USE biblioteca;
+
+CREATE TABLE autores (
+    ID_Autor INT(11) NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL,
+    Nacionalidad VARCHAR (50) NOT NULL,
+    Fecha_Nacimiento DATE NOT NULL,
+    PRIMARY KEY (ID_Autor)
+);
