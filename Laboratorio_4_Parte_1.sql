6a5e4f9694c1f1cfee184b6840cf0d1db9247861
luis-jaramillo-talentotech <luis.jaramillo.ttv.356@unilibre.edu.co>
2024-11-06 20:39:23 -0500

    Create table 'estudiantes'

--- Laboratorio_4_Parte_1.sql
+++ Laboratorio_4_Parte_1.sql
@@ -7,3 +7,11 @@ CREATE TABLE cursos (
     N_Curso VARCHAR(50) NOT NULL,
     PRIMARY KEY (ID_cursos)
 );
+
+CREATE TABLE estudiantes (
+    ID_Estudiante INT(11) NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL,
+    Fecha_Nacimiento DATE NOT NULL,
+    Direccion VARCHAR(100) NOT NULL,
+    PRIMARY KEY (ID_Estudiante)
+);
