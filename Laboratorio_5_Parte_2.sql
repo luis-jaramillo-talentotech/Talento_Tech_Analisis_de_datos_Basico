ca2328c5c9ee65bb8c300a28ce02f69ffda50525
luis-jaramillo-talentotech <luis.jaramillo.ttv.356@unilibre.edu.co>
2024-11-11 11:02:47 -0500

    Delete a book

--- Laboratorio_5_Parte_2.sql
+++ Laboratorio_5_Parte_2.sql
@@ -7,3 +7,7 @@ UPDATE libros SET Disponible = 0 WHERE Titulo = 'Rayuela';
 ---1.2.
 
 UPDATE prestamos SET Fecha_Devolucion = '2024-11-06' WHERE ID_Prestamo = 1;
+
+---1.3.
+
+DELETE FROM libros WHERE Titulo = 'Kafka en la orilla';
